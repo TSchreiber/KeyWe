4f7b9c63a5dd068402ada2dd734f0fbfffdae52c
TSchreiber <24798607+TSchreiber@users.noreply.github.com>
2023-11-21 11:01:26 -0600

    added 'tokens' table

--- docker-entrypoint-initdb.d/schema.sql
+++ docker-entrypoint-initdb.d/schema.sql
@@ -3,3 +3,10 @@ CREATE TABLE `users` (
   `hashedPassword` varchar(100) NOT NULL,
   PRIMARY KEY (`email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `tokens` (
+  `token_id` VARCHAR(255) NOT NULL,
+  `exp` BIGINT NOT NULL,
+  `revoked` TINYINT NOT NULL default '0',
+  PRIMARY KEY (`token_id`),
+  INDEX `EXP` (`exp` ASC) VISIBLE);
